8aa122ce5e3fed958544c9da37e2dddc41852b37
Natalia <nflores.teoria@gmail.com>
2020-08-27 11:32:27 -0400

    Agrego menús desplegables, falta hacer funcionar el listener

--- script.js
+++ script.js
@@ -35,6 +35,18 @@ function validar (e) {
     // divDevolucion.style.backgroundColor = white;
 };
 
+//Función menú desplegable
+
+var selectElem = document.getElementById('select');
+var pElem = document.getElementById('p')
+
+// When a new <option> is selected
+selectElem.addEventListener('change', function() {
+  var index = selectElem.selectedIndex;
+  // Add that data to the <p>
+  pElem.innerHTML = 'selectedIndex: ' + index;
+})
+
 //Validaciones
 
 function valRut (v) {
